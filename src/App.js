7f71b790bc85984c77c30f51f5fa77cd30b9fddf
creativejojo26 <91924790+creativejojo26@users.noreply.github.com>
2021-10-06 19:14:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 				<p>
 					Hello Group Edit <code>src/App.js</code> and save to reload.
 				</p>
+				<p><h2>JoAnna is still consfued</h2></p>
 				<p>Noah was here.</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React

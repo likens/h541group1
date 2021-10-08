b42c885883bf7d958e48a85fff7a7fc86a82286e
Noah Miller <nmiller@gaugesmart.com>
2021-10-08 18:43:39 -0400

    Noah M - Adding Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LifeLine from './components/LifeLine';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import LifeLine from "./components/LifeLine";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-DH94BC13XN");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
 	<React.StrictMode>
 		<LifeLine />
 	</React.StrictMode>,
-	document.getElementById('root')
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
